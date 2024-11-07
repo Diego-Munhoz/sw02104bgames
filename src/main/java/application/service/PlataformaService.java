fe0f8be87385269880fc6f9dfae6e63ed6a6fa99
Diego-Munhoz <diemunhoz@gmail.com>
2024-11-07 17:48:07 +0000

    save-07.11.2024

--- src/main/java/application/service/PlataformaService.java
+++ src/main/java/application/service/PlataformaService.java
@@ -1,7 +1,11 @@
 package application.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Plataforma;
 import application.record.PlataformaDTO;
@@ -18,5 +22,25 @@ public class PlataformaService {
 
     public PlataformaDTO insertOne(PlataformaDTO plataforma) {
         return new PlataformaDTO(plataformaRepo.save(new Plataforma(plataforma)));
+    }
+
+    public void removeById(long id) {
+        if(!plataformaRepo.existsById(id)){
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Plataforma não encontrada"
+            );
+        }
+        plataformaRepo.deleteById(id);
+    }
+
+    public PlataformaDTO update(long id, PlataformaDTO plataforma) {
+        Optional<Plataforma> resultado = plataformaRepo.findById(id);
+        if(resultado.isEmpty()){
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Plataforma não encontrada"
+            );
+        }
+        resultado.get().setNome(plataforma.nome());
+        return new PlataformaDTO(plataformaRepo.save(resultado.get()));
     }
 }
